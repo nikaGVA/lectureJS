2d61b8f6ddad93261549e7574dcd6586319361e1
nikaGVA <91367550+nikaGVA@users.noreply.github.com>
2023-05-11 19:20:47 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lectureJS
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-ihfr5c)
---
+++ counter.js
@@ -0,0 +1,10 @@
+// console.log('\n');
+
+const print = () => {
+  console.log("ehheh")
+}
+
+print()
+
+
+// console.log('\n');

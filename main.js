3339517bd2f7eb8d42739bf2dfc72936016e570c
nikaGVA <91367550+nikaGVA@users.noreply.github.com>
2023-05-11 20:29:24 +0400

    1 file modified

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log('afaf');
+
+
+let x = "*";
+let arr = '';
+for(let i = 0; i < 5; i++){
+  arr+=x;
+  console.log(arr)
+}
+
+console.log('\n')
+
+// davaleba 2
+
+let cars = ['ford', 'opel', 'mitsubishi','BMW','mercedes'];
+let price = [2000, 10000, 15000, 1700, 12340];
+
+let myArr = [];
+for(let i = 0; i<5; i++){
+  myArr[i] = {
+    "car":cars[i],
+    "price":price[i]
+  }
+}
+console.log(myArr)
+
+console.log("\n")
+
+const filter = myArr.filter( filtr => filtr.price < 10000 );
+console.log(filter)
+
+console.log('\n')
+
+
+
+myArr.map((data) => {
+  let saleprice = data.price + 90/100;
+  return [...data,...saleprice]
+})
+console.log(myArr)
